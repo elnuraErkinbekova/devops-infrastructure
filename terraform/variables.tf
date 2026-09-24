7457d80f8b1429215fceddfb5553960abad5cec2
elnuraErkinbekova <elnuraerkinbekova8@gmail.com>
2026-09-24 22:06:45 +0600

    feat(terraform): add input variables

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "AMI ID for the instance"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"
+}
